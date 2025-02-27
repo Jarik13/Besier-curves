6a2ed999a8e261eb612aa13c269af58a4e0899e7
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-27 16:15:41 +0200

    Create basic coord panel

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import panels.CartesianCoordinatePanel;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Besier curves");
+        initializeUI(frame);
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+    }
+
+    private static void initializeUI(JFrame frame) {
+        CartesianCoordinatePanel mainPanel = new CartesianCoordinatePanel();
+        frame.add(mainPanel, BorderLayout.CENTER);
     }
 }
--- src/panels/CartesianCoordinatePanel.java
+++ src/panels/CartesianCoordinatePanel.java
@@ -1,7 +1,26 @@
 package panels;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CartesianCoordinatePanel extends JPanel {
+    private int scale = 50;
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        int width = getWidth();
+        int height = getHeight();
+        int centerX = width / 2;
+        int centerY = height / 2;
+
+        g2d.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(1));
+
+        g2d.drawLine(0, centerY, width, centerY);
+        g2d.drawLine(centerX, 0, centerX, height);
+    }
 }
