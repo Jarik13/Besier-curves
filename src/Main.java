650d439539802e4bebb6ed7536a8fdb4c7960c5f
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-27 16:44:57 +0200

    Create input panel for manipulating curve points

--- src/Main.java
+++ src/Main.java
@@ -26,8 +26,28 @@ public class Main {
 
         contentPanel.add(tableScrollPane, BorderLayout.CENTER);
 
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JLabel xLabel = new JLabel("X:");
+        JTextField xField = new JTextField(5);
+        JLabel yLabel = new JLabel("Y:");
+        JTextField yField = new JTextField(5);
+        JButton addButton = new JButton("Add Point");
+        JButton clearButton = new JButton("Clear");
+        JButton createCurveButton = new JButton("Create Curve");
+
+        inputPanel.add(xLabel);
+        inputPanel.add(xField);
+        inputPanel.add(yLabel);
+        inputPanel.add(yField);
+        inputPanel.add(addButton);
+        inputPanel.add(clearButton);
+        inputPanel.add(createCurveButton);
+
         frame.setLayout(new BorderLayout());
         frame.add(mainPanel, BorderLayout.CENTER);
         frame.add(contentPanel, BorderLayout.WEST);
+        frame.add(inputPanel, BorderLayout.NORTH);
     }
 }
