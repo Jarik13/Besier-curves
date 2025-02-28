925ac481abc56c6287bcabfd6360504cda1a7994
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-28 17:52:57 +0200

    Update window ui. Split window into 2 parts - leftPanel and mainPanel

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import panels.CartesianCoordinatePanel;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.geom.Point2D;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,15 +16,13 @@ public class Main {
 
     private static void initializeUI(JFrame frame) {
         CartesianCoordinatePanel mainPanel = new CartesianCoordinatePanel();
-        JPanel contentPanel = new JPanel();
-        contentPanel.setLayout(new BorderLayout());
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
 
         String[] columnNames = {"Point", "X", "Y"};
         DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
         JTable pointTable = new JTable(tableModel);
         JScrollPane tableScrollPane = new JScrollPane(pointTable);
-
-        contentPanel.add(tableScrollPane, BorderLayout.CENTER);
 
         JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -72,9 +69,35 @@ public class Main {
         inputPanel.add(clearButton);
         inputPanel.add(createCurveButton);
 
+        JPanel bernsteinPanel = new JPanel();
+        bernsteinPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JLabel iLabel = new JLabel("i (Index):");
+        JTextField iField = new JTextField(5);
+        JLabel tStartLabel = new JLabel("start:");
+        JTextField tStartField = new JTextField(10);
+        JLabel tEndLabel = new JLabel("end:");
+        JTextField tEndField = new JTextField(10);
+        JButton calculateButton = new JButton("Calculate Bernstein");
+
+        bernsteinPanel.add(iLabel);
+        bernsteinPanel.add(iField);
+        bernsteinPanel.add(tStartLabel);
+        bernsteinPanel.add(tStartField);
+        bernsteinPanel.add(tEndLabel);
+        bernsteinPanel.add(tEndField);
+        bernsteinPanel.add(calculateButton);
+
+        JPanel tablePanel = new JPanel();
+        tablePanel.setLayout(new BorderLayout());
+        tablePanel.add(tableScrollPane, BorderLayout.CENTER);
+
+        leftPanel.add(inputPanel);
+        leftPanel.add(bernsteinPanel);
+        leftPanel.add(tablePanel);
+
         frame.setLayout(new BorderLayout());
         frame.add(mainPanel, BorderLayout.CENTER);
-        frame.add(contentPanel, BorderLayout.WEST);
-        frame.add(inputPanel, BorderLayout.NORTH);
+        frame.add(leftPanel, BorderLayout.WEST);
     }
 }
